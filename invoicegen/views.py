dfdfa80ab198964e39fdd269bd9916d96df84295
Халилов Рустам - LN <konversys@gmail.com>
2019-04-14 06:09:20 +0700

    Добавлена запись в файл. Добавлена генерация в POST запрос через массив Исправлены ошибки.

--- invoicegen/views.py
+++ invoicegen/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from invoicegen.treatment import write
+
 
 def invoice_send(request):
     try:
-        pass
+        if 'save' in request.POST:
+            write(request.POST.getlist('data_table'), request.POST.getlist('data_invoice'))
     finally:
         pass
     return render(request, 'invoice.html', locals())
